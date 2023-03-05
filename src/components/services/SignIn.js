893207674dc73cfa0dcfb6e0c8af12df8089b93d
Jose Monroy <mnry7999@gmail.com>
2023-03-05 16:47:49 -0600

    Implemenatcion de tokens

--- src/components/services/SignIn.js
+++ src/components/services/SignIn.js
@@ -1,9 +1,22 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const ENDPOINT_PATH = "https://reqres.in/api/";
 var LogIn = {};
 
 LogIn.iniciarSesion = function(usuario, contrasena) {
-    console.log("Iniciando sesión...");
-    console.log("Usuario: " + usuario);
-    console.log("Contraseña: " + contrasena);
+    const user = { usuario, contrasena };
+    return axios
+        .post(ENDPOINT_PATH + "login", user)
+        .then(response => {
+            Cookies.set("token", response.data.token);
+            return response.data;
+        }
+        )
+}
+
+LogIn.obtenerToken = function(){
+    return Cookies.get("token");
 }
 
 export default LogIn;
